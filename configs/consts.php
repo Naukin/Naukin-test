a18e575cb9239bb61b109c286ef9fbe2421a8c88
Naukin <34658674+Naukin@users.noreply.github.com>
2022-02-06 12:54:18 +0200

    Delete configs directory

--- configs/consts.php
+++
@@ -1,8 +0,0 @@
-<?php
-$DOMAIN = 'naukin.com';
-$_DOMAIN = "https://".$DOMAIN;
-$_SITE_TITLE = 'nd';
-$MAIN_COLOR = "#";
-$CACHE = "2";
-$SN_BANNER = "img/sn_share.png?".$CACHE;
-?>
--- configs/ru.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$__translations = [
-	    "d0"=>"",
-	];
-?>
